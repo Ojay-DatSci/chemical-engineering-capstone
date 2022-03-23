d91029e286a7740456f798719ac6437c82fad637
Ojay-DatSci <102194096+Ojay-DatSci@users.noreply.github.com>
2022-03-23 01:46:01 +0100

    Create testchild.py
    testing the child 1 2 !

---
+++ testchild.py
@@ -0,0 +1,9 @@
+Shey bit e don loss
+no loud it oh
+ma ti fere don lo
+doon't let your mummy know
+shey bit e don loss ai ai yaya ai
+my money don go and your money don go
+our money don go ahhhh
+our money don go go go go
+go go go my ma go go go go...
